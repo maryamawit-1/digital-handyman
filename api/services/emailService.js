57cd3a0434ef8dac8ee2dce8e64cd4ff94b772aa
maryamawit-1 <maryamawitlemma175@gmail.com>
2026-01-28 08:29:59 +0300

    fix gmail smtp config and email sending

--- api/services/emailService.js
+++ api/services/emailService.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // IMPORTANT
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 // 1. Application Received (Acknowledgment to Provider)
